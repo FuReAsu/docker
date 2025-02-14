57f76d25f5325f60c7543dd49366dc93fda52433
FuReAsu <maintain1346@gmail.com>
2025-02-14 15:30:45 +0630

    fixed storage_test.py

--- flaskrw/app/storage_test.py
+++ flaskrw/app/storage_test.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
 FILE_PATH = os.getenv('DATA_PATH','data/data.txt')
 
+if not os.path.exists(FILE_PATH):
+    with open(FILE_PATH, "w") as f:
+        pass
+    print(f"data file created at {FILE_PATH}")
+else:
+    print (f"data file already exists")
+
 HTML_PAGE = '''
 <!DOCTYPE html>
 <html lang="en">
